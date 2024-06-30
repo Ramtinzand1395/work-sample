f0a0ed73732cb4905d8cf704ac2c6be568833aff
ramtin.zand <ramtinzand6@gmail.com>
2024-06-30 17:41:39 +0330

    ramtin 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import header from "../assets//header.jpg";
+import header from "../assets/header.jpg";
 import Navbar from "./navbar/Navbar";
 const Header = () => {
   return (
-    <div className="w-full h-[80vh] bg-blue-700">
-      <div className=" font-tanha grid grid-cols-2 items-center ">
+    <div className="w-full h-[80vh] ">
+      <div className=" grid grid-cols-2 items-center ">
         <div className="flex flex-col items-start justify-evenly h-[80vh] text-white mx-auto">
-          <h2 className="font-bold text-2xl font-tanha">
-            هزینه طراحی وب سایت 1403
-          </h2>
-          <p className="font-vazir">
+          <h2 className="font-bold text-2xl font-t ">هزینه طراحی وب سایت 1403</h2>
+          <p>
             اگر بزینس خودتو داری و میخوای به درآمادم بالا برسی همین الان شروع کن
           </p>
         </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,46 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/js.png"
 const Navbar = () => {
+  const [navState, setNavState] = useState(false);
+  const onNavScroll = () => {
+    if (window.scrollY > 30) {
+      setNavState(true);
+    } else {
+      setNavState(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onNavScroll);
+    return () => {
+      window.removeEventListener("scroll", onNavScroll);
+    };
+  }, []);
   return (
-    <div className="w-full h-[20vh] flex items-center justify-around bg-blue-400 ">
-      <div className="">logo</div>
-      <div className=" text-white font-semibold font-vazir cursor-pointer flex items-center justify-around w-[60vw]">
-          <p className="hover:text-red-500 transition-all duration-150">
-            خدمات
-          </p>
-          <p className="hover:text-red-500 transition-all duration-150">
-            قیمت ها
-          </p>
-          <p className="hover:text-red-500 transition-all duration-150">
-            مقالات
-          </p>
-          <p className="hover:text-red-500 transition-all duration-150">
-            نمونه کار ها
-          </p>
+    <div
+      className={`fixed top-0 w-full h-[15vh] flex items-center justify-around bg-blue-400 transition-all duration-200 ${
+        navState ? "bg-blue-700 bg-opacity-80" : "bg-blue-400"
+      }`}
+    >
+      <div className="">
+        <img src={logo} className="w-10 h-10" alt="" />
+      </div>
+      <div className=" text-white text-xl font-vazir cursor-pointer flex items-center justify-around w-[60vw]">
+        <p className="hover:text-red-500 transition-all duration-150">خدمات</p>
+        <p className="hover:text-red-500 transition-all duration-150">
+          قیمت ها
+        </p>
+        <p className="hover:text-red-500 transition-all duration-150">مقالات</p>
+        <p className="hover:text-red-500 transition-all duration-150">
+          نمونه کار ها
+        </p>
       </div>
       <div className="w-auto flex items-center border-2 border-white rounded-md">
         <div className="bg-white p-4">
-          <p>ثبت سفارش </p>
+          <p className="text-blue-500 font-semibold font-tanha">ثبت سفارش </p>
         </div>
-        <div className="mx-2">
-          <p>09138433385</p>
+        <div className="mx-2 flex flex-col ">
+          <p className="hover:text-white cursor-pointer text-white font-bold">09138433385</p>
+          <p className="hover:text-white cursor-pointer text-white font-bold">09138433385</p>
         </div>
       </div>
     </div>
