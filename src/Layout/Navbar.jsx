33bc09ea9275447da3e4e2cc536ec32e2ce56f62
ramtin.zand <ramtinzand6@gmail.com>
2024-06-30 17:53:43 +0330

    ramtin 3

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../../assets/js.png"
+import logo from "../assets/js.png";
 const Navbar = () => {
   const [navState, setNavState] = useState(false);
   const onNavScroll = () => {
@@ -16,37 +16,43 @@ const Navbar = () => {
     };
   }, []);
   return (
-<<<<<<< HEAD:src/components/navbar/Navbar.tsx
     <div
-      className={`fixed top-0 w-full h-[15vh] flex items-center justify-around bg-blue-400 transition-all duration-200 ${
+      className={`fixed top-0 w-full h-[15vh] flex items-center justify-around transition-all duration-200 ${
         navState ? "bg-blue-700 bg-opacity-80" : "bg-blue-400"
       }`}
     >
-      <div className="">
-        <img src={logo} className="w-10 h-10" alt="" />
-      </div>
       <div className=" text-white text-xl font-vazir cursor-pointer flex items-center justify-around w-[60vw]">
-=======
-    <div className="w-full h-[20vh] flex items-center justify-around bg-blue-400 ">
-      <div className="">logo</div>
-      <div className=" text-white font-semibold font-vazir cursor-pointer flex items-center justify-around w-[60vw]">
->>>>>>> 94167fe7cbe465d0311446417b78e5d58e6afe2a:src/Layout/Navbar.jsx
-        <p className="hover:text-red-500 transition-all duration-150">خدمات</p>
-        <p className="hover:text-red-500 transition-all duration-150">
-          قیمت ها
-        </p>
-        <p className="hover:text-red-500 transition-all duration-150">مقالات</p>
-        <p className="hover:text-red-500 transition-all duration-150">
-          نمونه کار ها
-        </p>
+        <div className="w-full  flex items-center justify-around ">
+          <div className="">
+            <img src={logo} className="w-10 h-10" alt="" />
+          </div>
+          <div className=" text-white font-semibold font-vazir cursor-pointer flex items-center justify-around w-[60vw]">
+            <p className="hover:text-red-500 transition-all duration-150">
+              خدمات
+            </p>
+            <p className="hover:text-red-500 transition-all duration-150">
+              قیمت ها
+            </p>
+            <p className="hover:text-red-500 transition-all duration-150">
+              مقالات
+            </p>
+            <p className="hover:text-red-500 transition-all duration-150">
+              نمونه کار ها
+            </p>
+          </div>
+        </div>
       </div>
       <div className="w-auto flex items-center border-2 border-white rounded-md">
         <div className="bg-white p-4">
           <p className="text-blue-500 font-semibold font-tanha">ثبت سفارش </p>
         </div>
         <div className="mx-2 flex flex-col ">
-          <p className="hover:text-white cursor-pointer text-white font-bold">09138433385</p>
-          <p className="hover:text-white cursor-pointer text-white font-bold">09138433385</p>
+          <p className="hover:text-white cursor-pointer text-white font-bold">
+            09138433385
+          </p>
+          <p className="hover:text-white cursor-pointer text-white font-bold">
+            09138433385
+          </p>
         </div>
       </div>
     </div>
